2d5ecb01ba52741612479d085f1e1aecc710d0c5
Noah Treuhaft <noah.treuhaft@gmail.com>
2024-12-16 17:27:24 -0500

    Don't wrap errors in vector put operator (#5536)
    The sequence put operator passes errors along unmodified while the
    vector operator wraps them inside another error.  Make the vector
    implementation behave like the sequence implementation.

--- runtime/vam/expr/putter.go
+++ runtime/vam/expr/putter.go
@@ -23,7 +23,10 @@ func (p *Putter) Eval(vec vector.Any) vector.Any {
 
 func (p *Putter) eval(vecs ...vector.Any) vector.Any {
 	vec := vecs[0]
-	if vec.Type().Kind() != super.RecordKind {
+	if k := vec.Type().Kind(); k != super.RecordKind {
+		if k == super.ErrorKind {
+			return vec
+		}
 		return vector.NewWrappedError(p.zctx, "put: not a record", vec)
 	}
 	return p.recordExpr.Eval(vec)

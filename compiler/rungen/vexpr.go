2e17336430e6f273d4f2e6d7e21f1540fc6b8ac0
Matthew Nibecker <hello@mattnibecker.com>
2025-07-31 12:10:49 -0700

    Enable unnest expressions in vector runtime (#6083)

--- compiler/rungen/vexpr.go
+++ compiler/rungen/vexpr.go
@@ -62,6 +62,8 @@ func (b *Builder) compileVamExpr(e dag.Expr) (vamexpr.Evaluator, error) {
 		return b.compileVamSliceExpr(e)
 	case *dag.SetExpr:
 		return b.compileVamSetExpr(e)
+	case *dag.UnnestExpr:
+		return b.compileVamUnnestExpr(e)
 	//case *dag.MapCall:
 	//	return b.compileVamMapCall(e)
 	//case *dag.MapExpr:
@@ -406,4 +408,12 @@ func (b *Builder) compileVamListElems(elems []dag.VectorElem) ([]vamexpr.ListEle
 		}
 	}
 	return out, nil
+}
+
+func (b *Builder) compileVamUnnestExpr(unnest *dag.UnnestExpr) (vamexpr.Evaluator, error) {
+	e, err := b.compileUnnestExpr(unnest)
+	if err != nil {
+		return nil, err
+	}
+	return vamexpr.NewSamExpr(e), nil
 }

c555a8007a0cb9b80167fbc53fa9feb76f5d3ffd
Steven McCanne <steve@mccanne.com>
2025-09-23 07:21:43 -0700

    wip

--- compiler/semantic/analyzer.go
+++ compiler/semantic/analyzer.go
@@ -3,9 +3,7 @@ package semantic
 import (
 	"context"
 	"errors"
-	"slices"
 	"strconv"
-	"strings"
 
 	"github.com/brimdata/super"
 	"github.com/brimdata/super/compiler/ast"
@@ -40,28 +38,24 @@ func Analyze(ctx context.Context, p *parser.AST, env *exec.Environment, extInput
 	if extInput {
 		astseq.Prepend(&ast.DefaultScan{Kind: "DefaultScan"})
 	}
-	seq := t.semSeq(astseq)
-	if !HasSource(seq) {
+	semSeq := t.semSeq(astseq)
+	if !HasSource(semSeq) {
 		if t.env.IsAttached() {
-			if len(seq) == 0 {
+			if len(semSeq) == 0 {
 				return nil, errors.New("query text is missing")
 			}
-			seq.Prepend(&sem.NullScan{})
+			semSeq.Prepend(&sem.NullScan{})
 		} else {
 			// This is a local query and there's no external input
 			// (i.e., no command-line file args)
-			seq.Prepend(&sem.NullScan{})
+			semSeq.Prepend(&sem.NullScan{})
 		}
 	}
-	//XXX need daggen
-	seq = a.checkOutputs(true, seq)
-	r := newResolver(a)
-	seq, funcs := r.resolve(seq)
-	// Sort function entries so they are consistently ordered by integer tag strings.
-	slices.SortFunc(funcs, func(a, b *dag.FuncDef) int {
-		return strings.Compare(a.Tag, b.Tag)
-	})
-	return &dag.Main{Funcs: funcs, Body: seq}, files.Error()
+	r := newResolver(t)
+	semSeq, funcs := r.resolve(semSeq)
+	d := newDagen(t)
+	main := d.assemble(semSeq, funcs)
+	return main, files.Error()
 }
 
 // Translate AST into semantic tree.  Resolve all bindings
@@ -84,7 +78,6 @@ func newTranslator(ctx context.Context, files *srcfiles.List, env *exec.Environm
 	return &translator{
 		ctx:         ctx,
 		files:       files,
-		outputs:     make(map[*dag.Output]ast.Node),
 		env:         env,
 		scope:       NewScope(nil),
 		sctx:        super.NewContext(),
@@ -100,7 +93,7 @@ func HasSource(seq sem.Seq) bool {
 	switch op := seq[0].(type) {
 	case *sem.FileScan, *sem.HTTPScan, *sem.PoolScan, *sem.DBMetaScan, *sem.PoolMetaScan, *sem.CommitMetaScan, *sem.DeleteScan, *sem.NullScan:
 		return true
-	case *dag.Fork:
+	case *sem.ForkOp:
 		for _, path := range op.Paths {
 			if !HasSource(path) {
 				return false
@@ -160,53 +153,4 @@ func badOp() sem.Op {
 
 func (t *translator) error(n ast.Node, err error) {
 	t.files.AddError(err.Error(), n.Pos(), n.End())
-}
-
-// XXX this is for gendag XXX this is messed up
-// XXX this method should live on daggen
-func (t *translator) checkOutputs(isLeaf bool, seq dag.Seq) dag.Seq {
-	if len(seq) == 0 {
-		return seq
-	}
-	// - Report an error in any outputs are not located in the leaves.
-	// - Add output operators to any leaves where they do not exist.
-	lastN := len(seq) - 1
-	for i, o := range seq {
-		isLast := lastN == i
-		switch o := o.(type) {
-		case *dag.Output:
-			if !isLast || !isLeaf {
-				n, ok := t.outputs[o]
-				if !ok {
-					panic("system error: untracked user output")
-				}
-				t.error(n, errors.New("output operator must be at flowgraph leaf"))
-			}
-		case *dag.Scatter:
-			for k := range o.Paths {
-				o.Paths[k] = t.checkOutputs(isLast && isLeaf, o.Paths[k])
-			}
-		case *dag.Unnest:
-			o.Body = t.checkOutputs(false, o.Body)
-		case *dag.Fork:
-			for k := range o.Paths {
-				o.Paths[k] = t.checkOutputs(isLast && isLeaf, o.Paths[k])
-			}
-		case *dag.Switch:
-			for k := range o.Cases {
-				o.Cases[k].Path = t.checkOutputs(isLast && isLeaf, o.Cases[k].Path)
-			}
-		case *dag.Mirror:
-			o.Main = t.checkOutputs(isLast && isLeaf, o.Main)
-			o.Mirror = t.checkOutputs(isLast && isLeaf, o.Mirror)
-		}
-	}
-	switch seq[lastN].(type) {
-	case *dag.Output, *dag.Scatter, *dag.Fork, *dag.Switch, *dag.Mirror:
-	default:
-		if isLeaf {
-			return append(seq, &dag.Output{Name: "main"})
-		}
-	}
-	return seq
 }
--- compiler/semantic/dagen.go
+++ compiler/semantic/dagen.go
@@ -1,8 +1,11 @@
 package semantic
 
 import (
+	"errors"
+	"slices"
+	"strings"
+
 	"github.com/brimdata/super"
-	"github.com/brimdata/super/compiler/ast"
 	"github.com/brimdata/super/compiler/dag"
 	"github.com/brimdata/super/compiler/semantic/sem"
 )
@@ -10,33 +13,110 @@ import (
 type dagen struct {
 	//outputs map[*dag.Output]ast.Node // why point to Node?
 	outputs map[*dag.Output]any
+	funcs   map[string]*dag.FuncDef
+	t       *translator //XXX
 }
 
-func (d *dagen) compile(seq *sem.Main) *dag.Main {
+func newDagen(t *translator) *dagen {
+	return &dagen{
+		outputs: make(map[*dag.Output]any), //XXX any? sem.Any?
+		funcs:   make(map[string]*dag.FuncDef),
+		t:       t,
+	}
+}
+
+func (d *dagen) assemble(seq sem.Seq, funcs []*sem.FuncDef) *dag.Main {
+	dagSeq := d.seq(seq)
+	dagSeq = d.checkOutputs(true, dagSeq)
+	dagFuncs := make([]*dag.FuncDef, 0, len(d.funcs))
+	for _, f := range d.funcs {
+		dagFuncs = append(dagFuncs, f)
+	}
+	// Sort function entries so they are consistently ordered by integer tag strings.
+	slices.SortFunc(dagFuncs, func(a, b *dag.FuncDef) int {
+		return strings.Compare(a.Tag, b.Tag)
+	})
+	return &dag.Main{Funcs: dagFuncs, Body: dagSeq}
+}
+
+func (d *dagen) seq(seq sem.Seq) dag.Seq {
 	panic("TBD")
 }
 
-func evalAtCompileTime(sctx *super.Context, e sem.Expr) (super.Value, error) {
+func (d *dagen) expr(seq sem.Expr) dag.Expr {
 	panic("TBD")
 }
 
 // XXX
-func (d *dagen) debugOp(o *sem.DebugOp, mainAst ast.Seq, in sem.Seq) sem.Seq {
+func (d *dagen) debugOp(o *sem.DebugOp, mainSem sem.Seq, in dag.Seq) dag.Seq {
 	output := &dag.Output{Kind: "Output", Name: "debug"}
 	d.outputs[output] = o
 	e := d.expr(o.Expr)
 	y := &dag.Values{Kind: "Values", Exprs: []dag.Expr{e}}
-	main := d.seq(mainAst)
+	main := d.seq(mainSem)
 	if len(main) == 0 {
-		main.Append(&sem.Pass{Kind: "Pass"})
+		//XXX do we need pass?
+		main.Append(&dag.Pass{Kind: "Pass"})
 	}
 	return append(in, &dag.Mirror{
 		Kind:   "Mirror",
 		Main:   main,
-		Mirror: sem.Seq{y, output},
+		Mirror: dag.Seq{y, output},
 	})
 }
 
+func (d *dagen) checkOutputs(isLeaf bool, seq dag.Seq) dag.Seq {
+	if len(seq) == 0 {
+		return seq
+	}
+	// - Report an error in any outputs are not located in the leaves.
+	// - Add output operators to any leaves where they do not exist.
+	lastN := len(seq) - 1
+	for i, o := range seq {
+		isLast := lastN == i
+		switch o := o.(type) {
+		case *dag.Output:
+			if !isLast || !isLeaf {
+				//XXX
+				//n, ok := d.outputs[o]
+				//if !ok {
+				//	panic("system error: untracked user output")
+				//}
+				d.t.error(nil /*XXX*/, errors.New("output operator must be at flowgraph leaf"))
+			}
+		case *dag.Scatter:
+			for k := range o.Paths {
+				o.Paths[k] = d.checkOutputs(isLast && isLeaf, o.Paths[k])
+			}
+		case *dag.Unnest:
+			o.Body = d.checkOutputs(false, o.Body)
+		case *dag.Fork:
+			for k := range o.Paths {
+				o.Paths[k] = d.checkOutputs(isLast && isLeaf, o.Paths[k])
+			}
+		case *dag.Switch:
+			for k := range o.Cases {
+				o.Cases[k].Path = d.checkOutputs(isLast && isLeaf, o.Cases[k].Path)
+			}
+		case *dag.Mirror:
+			o.Main = d.checkOutputs(isLast && isLeaf, o.Main)
+			o.Mirror = d.checkOutputs(isLast && isLeaf, o.Mirror)
+		}
+	}
+	switch seq[lastN].(type) {
+	case *dag.Output, *dag.Scatter, *dag.Fork, *dag.Switch, *dag.Mirror:
+	default:
+		if isLeaf {
+			return append(seq, &dag.Output{Name: "main"})
+		}
+	}
+	return seq
+}
+
 func (d *dagen) exprNullable(e sem.Expr) dag.Expr {
+	panic("TBD")
+}
+
+func evalAtCompileTime(sctx *super.Context, e sem.Expr) (super.Value, error) {
 	panic("TBD")
 }

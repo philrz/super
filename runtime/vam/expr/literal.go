053b331cb3af660afa0273368af74e49ed85db44
Matthew Nibecker <hello@mattnibecker.com>
2026-01-09 15:33:54 -0800

    wip

--- runtime/vam/expr/literal.go
+++ runtime/vam/expr/literal.go
@@ -16,6 +16,25 @@ func NewLiteral(val super.Value) *Literal {
 	return &Literal{val: val}
 }
 
-func (l Literal) Eval(val vector.Any) vector.Any {
-	return vector.NewConst(l.val, val.Len(), bitvec.Zero)
+func (l Literal) Eval(vec vector.Any) vector.Any {
+	return buildLiteral(l.val, vec.Len())
+}
+
+func buildLiteral(val super.Value, n uint32) vector.Any {
+	typ := val.Type()
+	if named, ok := typ.(*super.TypeNamed); ok {
+		val := super.NewValue(named.Type, val.Bytes())
+		return vector.NewNamed(named, buildLiteral(val, n))
+	}
+	var nulls bitvec.Bits
+	if val.IsNull() {
+		nulls = bitvec.NewTrue(n)
+	}
+	if super.IsPrimitiveType(typ) {
+		return vector.NewConst(val, n, nulls)
+	}
+	b := vector.NewBuilder(typ)
+	b.Write(val.Bytes())
+	out := b.Build(nulls)
+	return vector.Pick(out, make([]uint32, n))
 }
